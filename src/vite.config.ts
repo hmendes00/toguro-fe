d917420abacf8fd53a1a0b527366a6ef9e82260a
hmendes <hmendes00@gmail.com>
2022-02-12 11:08:46 -0500

    removing vue-cli packages; updating vite config for production build

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,59 +5,63 @@ import { defineConfig } from 'vite';
 
 const resolvePath = (file: string) => resolve(__dirname, file);
 
-export default defineConfig({
-  server: {
-    port: 8080,
-    strictPort: true, // throw error if port in use
-    fs: {
-      strict: false
-    }
-  },
-  plugins: [
-    vue({
-      script: {
-        refSugar: true
-      },
-      template: {
-        compilerOptions: {
-          // treat any tag that starts with ion- as custom elements
-          isCustomElement: (tag) => tag.startsWith('toguro-') //stands for toguro custom app
-        }
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      port: 8080,
+      strictPort: true, // throw error if port in use
+      fs: {
+        strict: false
       }
-    }),
-    typescript({
-      target: 'es2020',
-      rootDir: resolvePath('.'),
-      declaration: true,
-      declarationDir: resolvePath('dist'),
-      exclude: resolvePath('node_modules/**'),
-      allowSyntheticDefaultImports: true
-    })
-  ],
-  resolve: {
-    dedupe: ['vue'],
-    alias: {
-      '@': resolvePath('./src'),
-      '@assets': resolvePath('./src/assets'),
-      '@styles': resolvePath('./src/assets/styles'),
+    },
+    plugins: [
+      vue({
+        script: {
+          refSugar: true
+        },
+        template: {
+          compilerOptions: {
+            // treat any tag that starts with ion- as custom elements
+            isCustomElement: (tag) => tag.startsWith('toguro-') //stands for toguro custom app
+          }
+        }
+      }),
+      typescript({
+        target: 'es2020',
+        rootDir: resolvePath('.'),
+        declaration: true,
+        declarationDir: resolvePath('dist'),
+        exclude: resolvePath('node_modules/**'),
+        allowSyntheticDefaultImports: true,
+        sourceMap: mode !== 'production',
+        inlineSources: mode !== 'production'
+      })
+    ],
+    resolve: {
+      dedupe: ['vue'],
+      alias: {
+        '@': resolvePath('./src'),
+        '@assets': resolvePath('./src/assets'),
+        '@styles': resolvePath('./src/assets/styles'),
 
-      // Views
-      '@components': resolvePath('./src/views/components'),
-      '@containers': resolvePath('./src/views/containers'),
-      '@pages': resolvePath('./src/views/pages'),
-      '@layouts': resolvePath('./src/views/layouts'),
+        // Views
+        '@components': resolvePath('./src/views/components'),
+        '@containers': resolvePath('./src/views/containers'),
+        '@pages': resolvePath('./src/views/pages'),
+        '@layouts': resolvePath('./src/views/layouts'),
 
-      // Services
-      '@services': resolvePath('./src/services'),
+        // Services
+        '@services': resolvePath('./src/services'),
 
-      // Helpers
-      '@helpers': resolvePath('./src/helpers'),
+        // Helpers
+        '@helpers': resolvePath('./src/helpers'),
 
-      // Helpers
-      '@models': resolvePath('./src/models'),
+        // Models
+        '@models': resolvePath('./src/models'),
 
-      // Store
-      '@store': resolvePath('./src/store')
+        // Store
+        '@store': resolvePath('./src/store')
+      }
     }
-  }
+  };
 });
